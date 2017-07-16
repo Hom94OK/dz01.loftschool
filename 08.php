70c0fde5a52aac3d2fd9a72ad1cdd751a30dfaaa
Hom94ok <khom.taras@gmail.com>
2017-07-16 18:25:37 +0300

    second commit

--- 08.php
+++ 08.php
@@ -1,14 +1,21 @@
 <link href="style.css" rel="stylesheet" media="all">
 Задача №8<br>
 <?php
-$str = 'My favorite phrase Hello world';
+$str = 'My favorite phrase Hello world !!!';
 echo $str;
 $strArr = explode(" ", $str);
 echo '<pre>';
 print_r($strArr);
 echo '</pre>';
+
 $x = 0;
-while ($x < count($strArr)) {
-    echo $strArr[$x];
+$strRevers = array_reverse($strArr);
+$cntStr = count($strRevers);
+while ($x < $cntStr) {
+    if ($x < $cntStr - 1) {
+        echo$strRevers[$x] . '-';
+    } else {
+        echo $strRevers[$x];
+    }
     $x++;
 }
